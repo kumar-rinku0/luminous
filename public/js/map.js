cb01bcc1727ade8fdbc43a443f4ed2beb31da8cd
rinku kumar <rinkukumar412616@gmail.com>
2024-12-02 18:30:43 +0530

    map marker added with popup

--- public/js/map.js
+++ public/js/map.js
@@ -5,7 +5,22 @@ const map = new mapboxgl.Map({
   center: newCoordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
   zoom: 12, // starting zoom
   style: "mapbox://styles/mapbox/streets-v12",
-});
+  attributionControl: false,
+}).addControl(
+  new mapboxgl.AttributionControl({
+    customAttribution: "Map design by me!",
+  })
+);
+
+map.addControl(
+  new mapboxgl.GeolocateControl({
+    positionOptions: {
+      enableHighAccuracy: true,
+    },
+    trackUserLocation: true,
+    showUserHeading: true,
+  })
+);
 
 // const marker1 = new mapboxgl.Marker({ color: "red" })
 //   .setLngLat([75.81248, 26.920341])
@@ -14,8 +29,22 @@ const map = new mapboxgl.Map({
 // value = coordinates.split(",");
 // console.log(value);
 
-const marker2 = new mapboxgl.Marker({ color: "red" })
+const popup = new mapboxgl.Popup({
+  className: "map-popup",
+  closeButton: true,
+  closeButton: true,
+  closeOnClick: true,
+  closeOnMove: true,
+})
   .setLngLat(newCoordinates)
+  .setHTML(`${locationValue}`)
   .addTo(map);
 
+const marker2 = new mapboxgl.Marker({ color: "red" })
+  .setLngLat(newCoordinates)
+  .setPopup(popup)
+  .addTo(map);
+
+// const map = new mapboxgl.Map({)
+// .addControl();
 // const marker1 = new mapboxgl.Marker().setLngLat().addTo(map);
